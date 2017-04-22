f66753d4184c1f513f39e2c0952e9368b4c03a96
javecs <moonhee.javecs@gmail.com>
2017-04-22 21:33:08 +0900

    JARファイルのリソースを実行中にロードできない問題を修正しました。

--- src/main/kotlin/xyz/javecs/calc/bot/utils/FunctionLoader.kt
+++ src/main/kotlin/xyz/javecs/calc/bot/utils/FunctionLoader.kt
@@ -1,20 +1,14 @@
 package xyz.javecs.calc.bot.utils
 
-import org.apache.commons.io.FileUtils
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.core.io.ResourceLoader
+import com.google.common.io.Resources
 import org.springframework.stereotype.Component
 import xyz.javecs.tools.expr.Function
-import java.nio.charset.Charset
 
 @Component
 class FunctionLoader {
-    @Autowired
-    lateinit var loader: ResourceLoader
-
     fun read(name: String): Function {
-        val resource = loader.getResource("classpath:/functions/$name.txt")
-        val lines = FileUtils.readLines(resource.file, Charset.defaultCharset())
+        val resource = Resources.getResource("functions/$name.txt")
+        val lines = Resources.toString(resource, Charsets.UTF_8).split(System.lineSeparator())
         return Function(lines.first(), lines.subList(1, lines.size).toTypedArray())
     }
 }
