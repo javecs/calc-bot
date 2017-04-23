793c54ba14e6777f3fc4ef0edcdb29ce7d7e895e
javecs <moonhee.javecs@gmail.com>
2017-04-23 23:08:52 +0900

    text2exprを使って計算しました。

--- src/main/kotlin/xyz/javecs/calc/bot/api/Messenger.kt
+++ src/main/kotlin/xyz/javecs/calc/bot/api/Messenger.kt
@@ -1,24 +1,21 @@
 package xyz.javecs.calc.bot.api
 
 import com.linecorp.bot.model.event.Event
-import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
-import com.linecorp.bot.spring.boot.annotation.EventMapping
-import com.linecorp.bot.model.message.TextMessage
-import com.linecorp.bot.model.event.message.TextMessageContent
 import com.linecorp.bot.model.event.MessageEvent
-import org.springframework.beans.factory.annotation.Autowired
-import xyz.javecs.calc.bot.utils.textToOperator
-import xyz.javecs.tools.expr.Calculator
+import com.linecorp.bot.model.event.message.TextMessageContent
+import com.linecorp.bot.model.message.TextMessage
+import com.linecorp.bot.spring.boot.annotation.EventMapping
+import com.linecorp.bot.spring.boot.annotation.LineMessageHandler
+import xyz.javecs.tools.text2expr.Text2Expr
 
 @LineMessageHandler
 class Messenger {
 
-    @Autowired
-    lateinit var calculator: Calculator
+    val text2Expr = Text2Expr()
 
     @EventMapping
     fun reply(event: MessageEvent<TextMessageContent>): TextMessage {
-        return TextMessage(calculator.eval(textToOperator(event.message.text)).value.toString())
+        return TextMessage(text2Expr.eval(event.message.text))
     }
 
     @EventMapping
